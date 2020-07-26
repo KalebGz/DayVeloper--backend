02b9ebcf6dab67c05167e2527378ba8f81826b6d
KalebGz <kaleb.zeray@gmail.com>
2020-07-26 02:40:25 -0400

    created and tested sample get request for taskcategories index

--- app/controllers/api/v1/task_categories_controller.rb
+++ app/controllers/api/v1/task_categories_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::TaskCategoriesController < ApplicationController
+    
+    def show
+        task_category = TaskCategory.find(params[:id])
+        render json: taskCategory
+    end
+
+    def index
+        task_categories = TaskCategory.all
+        render json: task_categories
+    end
 end
